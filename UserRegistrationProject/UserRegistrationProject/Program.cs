048c77633e13116345437d25e13c9ae9e99de858
ronit#123 <ronitwaje23>
2022-02-14 20:15:16 +0530

    adding First name

--- UserRegistrationProject/UserRegistrationProject/Program.cs
+++ UserRegistrationProject/UserRegistrationProject/Program.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserRegistrationProject
+namespace UserRegistrationexRefAnn
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to User Registration Project");
+            Console.WriteLine("Welcome to User Registration Program");
+            UserRegistration();
             Console.ReadLine();
-
+        }
+        //Method to perform UserRegistration
+        public static void UserRegistration()
+        {
+            /* UC-1-UserRegistration
+             * Validate Fisrt Name starts with Cap and have atlest 2 letters.
+             */
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(UserRegister.ValidateFirstName(firstName));
         }
     }
 }
---
+++ UserRegistrationtest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserRegistrationtest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserRegistrationtest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c347ba33-c213-4446-8c7b-a7cce0115d0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
